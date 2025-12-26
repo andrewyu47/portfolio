44cc1c31395e5f981e41f2ee0bdb2f10f7e30ede
Andrew <andrewy@cisco.com>
2025-12-26 01:19:37 -0800

    dpro

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
                 <div className={styles.buttons}>
                   <Link
                     className="button button--primary button--lg"
-                    to="/docs/case-studies/sage"> 
+                    to="/docs/case-studies/project-sage"> 
                     View Case Study
                   </Link>
                 </div>
