6e43bf43f3b6e1f70ced9c22e8714fa1e98ec157
Andrew <andrewy@cisco.com>
2025-12-26 02:01:27 -0800

    page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -73,7 +72,6 @@ export default function Home() {
           </div>
         </section>
 
-        <HomepageFeatures /> 
       </main>
     </Layout>
   );
