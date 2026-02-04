ca11918d3b31bd9330e1c9454ae5461f97ecae6f
andrewyu47 <andrewyu47@gmail.com>
2026-02-03 23:02:35 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">
           {/* Added self-closing syntax <br /> for React best practice */}
-          Senior Technical Writer and AI Manager<br /> applying First Principles to AI & Language.
+          Product Manager and Senior Technical Writer<br /> applying First Principles to AI & Language.
         </h1>
         <p className={clsx('hero__subtitle', styles.heroSubtitle)}>
           I deconstruct complex user problems to their root causes, leveraging Systems Theory 
